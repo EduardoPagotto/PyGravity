39708c67f12aff494f1e2aab35774750e5286169
Eduardo Pagotto <edupagotto@gmail.com>
2020-02-01 16:08:31 -0300

    Quadtree com leaf como opcional

--- PyGravity/render/QuadTree.py
+++ PyGravity/render/QuadTree.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 '''
 Created on 20191109
-Update on 20200130
+Update on 20200201
 @author: Eduardo Pagotto
  '''
 
@@ -13,9 +13,11 @@ from random import randint
 from Rectangle import Rectangle
 
 class QuadTree(object):
-    def __init__(self, boundary, capacity):
+    def __init__(self, boundary, capacity, leafyMode=False, deep=0):
         self.boundary = boundary
         self.capacity = capacity
+        self.leafyMode = leafyMode
+        self.deep = deep
         self.points = []
         self.divided = False
         self.nw = None
@@ -30,7 +32,9 @@ class QuadTree(object):
 
         p = self.boundary.pos
         s = self.boundary.size / 2
-        
+        new_deep = self.deep + 1
+        self.divided = True
+
         # FIXME: considera a tela com 0,0 em top-left (opengl e botton-left)
         # para opengl trocar north com south
         ne = Rectangle(glm.vec2(p.x + s.x, p.y - s.y), s)
@@ -38,38 +42,48 @@ class QuadTree(object):
         se = Rectangle(glm.vec2(p.x + s.x, p.y + s.y), s)
         sw = Rectangle(glm.vec2(p.x - s.x, p.y + s.y), s)
 
-        self.ne = QuadTree(ne, self.capacity)
-        self.nw = QuadTree(nw, self.capacity)
-        self.se = QuadTree(se, self.capacity)
-        self.sw = QuadTree(sw, self.capacity)
+        self.ne = QuadTree(ne, self.capacity, self.leafyMode, new_deep)
+        self.nw = QuadTree(nw, self.capacity, self.leafyMode, new_deep)
+        self.se = QuadTree(se, self.capacity, self.leafyMode, new_deep)
+        self.sw = QuadTree(sw, self.capacity, self.leafyMode, new_deep)
 
-        self.divided = True
-        
+    def __insert_new(self, point):
+        if self.ne.insert(point):
+            return True
+
+        if self.nw.insert(point):
+            return True
+
+        if self.se.insert(point):
+            return True
+
+        if self.sw.insert(point):
+            return True
+
 
     def insert(self, point):
 
         if self.boundary.contains(point) is not True:
             return False
 
-        if len(self.points) < self.capacity:
+        # by Karnaugh
+        A = len(self.points) < self.capacity
+        B = self.leafyMode is False
+        C = self.divided is False
+
+        if (A and B) or (A and C):
             self.points.append(point)
             return True
         else:
             if self.divided is False:
                 self.subdivide()
-                
-            if self.ne.insert(point):
-                return True
+                    
+            if self.leafyMode is True:
+                for p in self.points:
+                    self.__insert_new(p)
+                    self.points = []
 
-            if self.nw.insert(point):
-                return True
-
-            if self.se.insert(point):
-                return True
-
-            if self.sw.insert(point):
-                return True
-        
+            return self.__insert_new(point)
 
     def query(self, retangle, found):
         if not self.boundary.intersects(retangle):
@@ -91,20 +105,16 @@ def randomic(boundary):
     return glm.vec2(randx, randy)
 
 if __name__ == '__main__':
-    
     # seed(1)
     # boundary = Rectangle(glm.vec2(200, 200), glm.vec2(200, 200))
-
-    # qt = QuadTree(boundary, 4)
-
-    # for _ in range(50):
+    # qt = QuadTree(boundary, 1, True)
+    # for _ in range(150):
     #     qt.insert(randomic(boundary))
-
     # print(str(qt))
 
     boundary = Rectangle(glm.vec2(20,20), glm.vec2(20,20))
 
-    qt = QuadTree(boundary, 1)
+    qt = QuadTree(boundary, 1, True)
 
     for i in range(4):
         for j in range(4):
@@ -113,6 +123,6 @@ if __name__ == '__main__':
 
             qt.insert(glm.vec2(x,y))
 
-    print('fim')
+    qt.insert(glm.vec2(7,18))
 
-    
+    print('fim')
